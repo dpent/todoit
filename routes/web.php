2c2b95fa36457f132db0ef9097f371e77dce8334
dpent <dim.pentaris@gmail.com>
2025-07-10 13:26:22 +0300

    Added login&register

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,6 +11,8 @@ Route::get('/', function () {
 Route::get('/login', function () {
     return view('login');
 });
+
+Route::post('/login',[LoginController::class,'authenticate']);
 
 Route::get('/todoList', function () {
     return view('todoList');
@@ -21,3 +25,5 @@ Route::get('/profile', function () {
 Route::get('/signup', function () {
     return view('signup');
 });
+
+Route::post('/signup',[RegisterController::class,'register']);
